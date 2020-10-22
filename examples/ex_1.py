d9b93c657a36218afa1b8808c92686f76ebc8091
David Lignell <dol4@ash.et.byu.edu>
2020-10-22 17:14:57 -0600

    initial writing of parallel_planes.py and ex_1.py. Still need to test and debug

---
+++ examples/ex_1.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from parallel_planes import parallel_planes, sigma
+
+sys.path.append('/Users/dol4/codes/radlib/installed/lib/python3.8/site-packages')
+
+from pyrad import pyrad_planck_mean as rad_planck_mean
+from pyrad import pyrad_wsgg        as rad_wsgg
+from pyrad import pyrad_rcslw       as rad_rcslw
+
+################################################################################
+
+#--------------------- parameters
+
+P      = 101325.0
+Thot   = 2000
+Tcold  = 300
+Lhot   = 0.5
+nGG    = 24
+ntheta = 101
+nxh    = 1001    # for Lcold=0 changes below to keep dx roughly constant
+
+Lcold  = np.array([0, 0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0, 1.5, 2.0])
+
+nGGa   = nGG+1
+
+xco2=0.1; xco=0.0; xh2o=0.2; xch4=0.0; fvs=0.0
+
+#---------------------
+
+print("# Lcold (m), q(L)/σThot^4")
+
+for iLcold in range(len(Lcold)):
+
+    L  = Lcold[iLcold] + Lhot
+
+    Tavg = (Thot*0.5+Tcold*Lcold[iLcold])/L
+
+    nx = int(nxh*L/Lhot)
+    x = np.linspace(0,L,nx)
+
+    xCO2   = np.full(nx, xco2)
+    xCO    = np.full(nx, xco)
+    xH2O   = np.full(nx, xh2o)
+    xCH4   = np.full(nx, xch4)
+    fvsoot = np.full(nx, fvs)
+
+    T = np.full(nx,Thot)
+    T[x>Lhot] = Tcold
+
+    rcslw = rad_rcslw(nGG, P, Tavg, xh2o, xco2, xco, fvs)
+
+    #--------------------- get q, Q
+
+    x, xQ, q, Q = parallel_planes(rcslw, L, ntheta, P, T, xH2O, xCO2, xCO, xCH4, fvsoot, true)
+
+    #-------------------------------------------------------------------------
+
+    print(f'\n{Lcold[iLcold]}  {q[nx-1]/sigma/Thot**4}')
+
+print()
+
---
+++ examples/parallel_planes.py
@@ -0,0 +1,130 @@
+################################################################################
+
+import numpy as np
+from typing import Tuple, Any
+from nptyping import NDArray
+
+sigma = 5.670367E-8     # Stephan-Boltzmann constant
+
+def I_IT(x:     NDArray[Any],
+         theta: float, 
+         T:     NDArray[Any],
+         kabs:  NDArray[Any,Any],
+         awts:  NDArray[Any,Any],
+         Ilo:   NDArray[Any],
+         Ihi:   NDArray[Any]) -> NDArray[Any,Any]
+    '''
+    Compute intensity at each grid point for a given angle.
+    Solution using an implicit trapazoid method.
+    @param x     \input vector of grid points
+    @param theta \input angle measured: grid between plates aligned with theta=0
+    @param T     \input vector of temperatures (K)
+    @param kabs  \input 2D vector of absorption coefficients for each gas [igrid, igas]
+    @param awts  \input 2D vector of weights for each gas [igrid, igas]
+    @param Ilo   \input vector of gas intensities on the lower plane.
+    @param Ihi   \input vector of gas intensities on the upper plane.
+    @param I     \ouput vector of vector of gas intensities in direction theta [igrid, igas]
+    '''
+
+    n     = len(x)
+    nGGa  = len(kabs[0,:])
+
+    double Ib1, Ib2
+    mu = np.abs(np.cos(theta))
+
+    I = np.empty((nx, nGGa))
+
+    if(theta <= np.pi/2):
+        I[0] = Ilo
+        for i in range(n-1):     
+            Ib1 = sigma/np.pi*T[i+1]**4
+            Ib2 = sigma/np.pi*T[i]**4
+            dx  = x[i+1] - x[i]
+            for j in range(nGGa):
+                I[i+1,j] = (I[i,j] + dx/mu*0.5*(kabs[i+1,j]*awts[i+1,j]*Ib1 + kabs[i,j]*(awts[i,j]*Ib2 - I[i,j]))) /
+                            (1.0+dx/mu*0.5*kabs[i+1,j])
+    else:
+        I[n-1] = Ihi
+        for i in range(n-1,0,-1):
+            Ib1 = sigma/np.pi*T[i-1]**4
+            Ib2 = sigma/np.pi*T[i]**4
+            dx  = x[i] - x[i-1]
+            for j in range(nGGa):
+                I[i-1,j] = (I[i,j] + dx/mu*0.5*(kabs[i-1,j]*awts[i-1,j]*Ib1 + kabs[i,j]*(awts[i,j]*Ib2 - I[i,j]))) /
+                            (1.0+dx/mu*0.5*kabs[i-1,j])
+
+    return I
+
+################################################################################
+
+parallel_planes(RAD,
+                L:        float,
+                ntheta:   int, 
+                P:        float, 
+                T:        NDArray[Any],
+                xH2O:     NDArray[Any],
+                xCO2:     NDArray[Any],
+                xCO:      NDArray[Any],
+                xCH4:     NDArray[Any],
+                fvsoot:   NDArray[Any],
+                LzeroIbc: bool =false) -> Tuple[NDArray[Any], NDArray[Any], NDArray[Any], NDArray[Any]]:
+    '''
+    Compute radiative heat flux (q) and volumetric heat source (Q) profiles between parallel planes.
+    Solution using simple ray tracing.
+    Pass in T, P, composition profiles on equally spaced points including boundaries.
+    @param RAD      \input radlib object
+    @param L        \input distance (m) between planes
+    @param ntheta   \input number of ray angles
+    @param P        \input system pressure (Pa)
+    @param T        \input vector of temperatures at each point (K)
+    @param xH2O     \input vector of H2O mole fractions at each point
+    @param xCO2     \input vector of CO2 mole fractions at each point
+    @param xCO      \input vector of CO  mole fractions at each point
+    @param xCH4     \input vector of CH4 mole fractions at each point
+    @param fvsoot   \input vector of soot volume fractions at each point
+    @param q        \output vector of heat fluxes at each point (W/m2)
+    @param x        \output vector of grid locations (m)
+    @param xQ       \output vector of Q grid locations (m)
+    @param LzeroIbc \input bool true to make Ibc=0, false otherwise (default = false)
+     '''
+
+    #--------------------- set the grid of x and array of theta
+
+    dtheta = np.pi/ntheta
+
+    theta = np.linspace(dtheta/2, np.pi-dtheta/2, ntheta) 
+
+    nx = len(T)
+    dx = L/(nx-1)
+    x = np.linspace(0,L,nx)               # x grid are face values:   |   |   |   |   | 
+    xQ = 0.5*(x[:-1] + x[1:])             # xQ grid are cell centers:   *   *   *   *
+
+    #--------------------- initialize radiative properties
+
+    kabs = np.empty((nx, Rad.get_nGG))
+    awts = np.empty_like(kabs)
+
+    for i in range(nx):
+        kabs[i,:], awts[i,:] = RAD.get_k_a(T[i], P, xH2O[i], xCO2[i], xCO[i], xCH4[i], fvsoot[i])
+
+    if LzeroIbc:
+        Ilo = np.zeros(RAD.get_nGGa())
+        Ihi = np.zeros(RAD.get_nGGa())
+    else:
+        Ilo = sigma/np.pi*T[0] **4*awts[0,:]
+        Ihi = sigma/np.pi*T[-1]**4*awts[-1,:]
+
+    #--------------------- solve for q, Q
+
+    q = np.zeros(nx)
+    Q = np.empty(nx-1)
+
+    for j in range(ntheta):
+        I = I_IT(x, theta[j], T, kabs, awts, Ilo, Ihi)
+        for(int i=0; i<nx; ++i):
+            q[i] += 2.0*np.pi*dtheta*np.cos(theta[j])*np.sin(theta[j])*np.sum(I[i,:])
+
+    Q = -(q[1:]-q[:-1])/dx
+
+    return x, xQ, q, Q
+
