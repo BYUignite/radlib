022e34dbd31f231f4822ba26baff1b67a747cf26
David Lignell <dol4@ash.et.byu.edu>
2020-11-20 11:03:00 -0700

    updating cmake for building c++ examples

--- examples/c++/make_and_run_examples.sh
+++ examples/c++/make_and_run_examples.sh
@@ -5,12 +5,12 @@ set -x
 include="../../installed/include"
 lib="../../installed/lib"
 
-g++ -std=c++11 -I$include -L$lib simple_interface.cc -lradlib -o simple_interface.x; 
-g++ -std=c++11 -I$include -L$lib ex_S1.cc parallel_planes.cc -lradlib -o ex_S1.x; 
-g++ -std=c++11 -I$include -L$lib ex_S2.cc parallel_planes.cc -lradlib -o ex_S2.x; 
-g++ -std=c++11 -I$include -L$lib ex_S3.cc parallel_planes.cc -lradlib -o ex_S3.x; 
-g++ -std=c++11 -I$include -L$lib ex_S4.cc parallel_planes.cc -lradlib -o ex_S4.x; 
-g++ -std=c++11 -I$include -L$lib ex_S5.cc parallel_planes.cc -lradlib -o ex_S5.x; 
+g++ -O3 -std=c++11 -I$include -L$lib simple_interface.cc -lradlib -o simple_interface.x; 
+g++ -O3 -std=c++11 -I$include -L$lib ex_S1.cc parallel_planes.cc -lradlib -o ex_S1.x; 
+g++ -O3 -std=c++11 -I$include -L$lib ex_S2.cc parallel_planes.cc -lradlib -o ex_S2.x; 
+g++ -O3 -std=c++11 -I$include -L$lib ex_S3.cc parallel_planes.cc -lradlib -o ex_S3.x; 
+g++ -O3 -std=c++11 -I$include -L$lib ex_S4.cc parallel_planes.cc -lradlib -o ex_S4.x; 
+g++ -O3 -std=c++11 -I$include -L$lib ex_S5.cc parallel_planes.cc -lradlib -o ex_S5.x; 
 
 printf "\n\n********** Simple Interface\n"
 ./simple_interface.x
