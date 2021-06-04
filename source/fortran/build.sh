b27b1277035ec07d7ca402fe2c2ff90aff683efa
David Lignell <davidlignell@byu.edu>
2021-06-04 15:43:44 -0600

    fortran interface completed: cmake inclusion; tested

--- source/fortran/build.sh
+++
@@ -1,7 +0,0 @@
-
-g++ -std=c++11 -I/Users/dol4/codes/radlib/installed/include -c c_interface.cc
-
-gfortran -c rad_module.f90
-gfortran -c main.f90
-
-gfortran -o test.x -L/Users/dol4/codes/radlib/installed/lib/ main.o rad_module.o c_interface.o -lc++ -lradlib
